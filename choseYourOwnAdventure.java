0a8535dfc8fe90cf484d19d14fa3603aa2d8ff35
Gyberg <jakeg04589@stu.powayusd.com>
2022-10-10 14:19:34 -0700

    test2

--- choseYourOwnAdventure.java
+++ choseYourOwnAdventure.java
@@ -1,60 +1,153 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
- 
  
 public class choseYourOwnAdventure {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
  
         Scanner input = new Scanner(System.in);
-        
-        List<String> choices = new ArrayList<>();
 
-        System.out.println("Do you want to go outside or stay indide for recess? \n a for inside, b for outside");
+        // asks user the first question
+        System.out.println("Do you want to go outside or stay inside for recess? \n a for inside, b for outside"); 
         String input1 = input.nextLine();
-        choices.add(input1);
-        System.out.print(choices);
-   
-        if (input1 == "a") {
+        
+        if (input1 == "a") 
+        {
             System.out.println("Do you want to eat a snack or play a game? \n a for snack, b for play game");
-            input1 = input.nextLine();
-            choices.add(input1);
-            if(input1 == "a")
+            String input2 = input.nextLine();
+
+            if(input2 == "a")
             {
                 System.out.println("Do you want to eat fruits or chips? \n a or fruits, b for chips");
-                input1 = input.nextLine();
-                choices.add(input1);
-                if (input1 == "a") {
-                    //Final statment
-                } else if (input1 == "b") {
-                    //Final statment
+                String input3 = input.nextLine();
+                if (input3 == "a") 
+                {
+                    if(input1 == "a" && input2 == "a" && input3 == "a")
+                    {
+                        System.out.println("You chose to stay inside and eat fruits for your snack!");
+                    }
+                } 
+                else if (input3 == "b") 
+                {
+                    if(input1 == "a" && input2 == "a" && input3 == "b")
+                    {
+                        System.out.println("You chose to stay inside and eat chips for your snack!");
+                    }
+                }
+                else 
+                {
+                    System.out.println("Do you want to eat fruits or chips? \n a or fruits, b for chips");
+                    input3 = input.nextLine();
+                    main(new String[0]); //goes back to the beginning of sequence
                 }
 
-            } else if (input1 == "b") {
-
+            } 
+            else if (input2 == "b") 
+            {
+                System.out.println("Do you want to play cards or video games? \n a or cards, b for video games");
+                String input4 = input.nextLine();
+                if (input4 == "a") 
+                {
+                    if(input1 == "a" && input2 == "b" && input4 == "a")
+                    {
+                        System.out.println("You chose to stay inside and play cards for your game!");
+                    }               
+                } 
+                else if (input4 == "b") 
+                {
+                    if(input1 == "a" && input2 == "b" && input4 == "b")
+                    {
+                        System.out.println("You chose to stay inside and play videogames for your game!");
+                    }  
+                }
+                else 
+                {
+                    System.out.println("Do you want to play cards or video games? \n a or cards, b for video games");
+                    input4 = input.nextLine();
+                    main(new String[0]); //goes back to the beginning of sequence
+                }
             }
  
             
-        } else if (input1 == "b") {
-            System.out.println("Jumprope, basketball or draw");
-            input1 = input.nextLine();
-            choices.add(input1);
-            if (input1 == "a") {
-                input1 = input.nextLine();
-                choices.add(input1);
-                if (input1 == "c") {
-
-                } else if (input1 == "b") {
-                    
+        } 
+        else if (input1 == "b") 
+        {
+            System.out.println("a for Jumprope, b for basketball or c for draw");
+            String input5 = input.nextLine();
+ 
+            if (input5 == "a") 
+            {
+                System.out.println("Do you want to jumprope by yourself or with a friend? \n a alone or b with friend");
+                String input6 = input.nextLine();
+                if (input6 == "a") 
+                {
+                    if(input1 == "b" && input5 == "a" && input6 == "a")
+                    {
+                        System.out.println("You chose to go outside and jumprope by yourself!");
+                    }
+                } 
+                else if (input6 == "b")
+                {
+                    if(input1 == "b" && input5 == "a" && input6 == "b")
+                    {
+                        System.out.println("You chose to go ouside and jumprope with a friend!");
+                    }
                 }
-            } else if (input1 == "b") {
-                input1 = input.nextLine();
-                choices.add(input1);
-
-            } else if (input1 == "c") {
-                input1 = input.nextLine();
-                choices.add(input1);
-
+                else 
+                {
+                    System.out.println("Do you want to jumprope by yourself or with a friend? \n a alone or b with friend");
+                    input6 = input.nextLine();
+                    main(new String[0]); //goes back to the beginning of sequence
+                }
+            } 
+            else if (input5 == "b") 
+            {
+                System.out.println("Do you want to play 1v1 (a) or on a team (b)?");
+                String input7 = input.nextLine();
+                if (input7 == "a") 
+                {
+                    if(input1 == "b" && input5 == "b" && input7 == "a")
+                    {
+                        System.out.println("You chose to go outside and play 1v1 basketball!");
+                    }
+                } 
+                else if (input7 == "b")
+                {
+                    if(input1 == "b" && input5 == "b" && input7 == "b")
+                    {
+                        System.out.println("You chose to go outside and play basketball with a team!");
+                    }
+                }
+                else 
+                {
+                    System.out.println("Do you want to play 1v1 (a) or on a team (b)?");
+                    input7 = input.nextLine();
+                    main(new String[0]); //goes back to the beginning of sequence
+                }
+            } 
+            else if (input5 == "c") 
+            {
+                System.out.println("Do you want to draw with crayons (a) or colored pencils (b)?");
+                 String input8 = input.nextLine();
+                if (input8 == "a") 
+                {
+                    if(input1 == "b" && input5 == "c" && input8 == "a")
+                    {
+                        System.out.println("You chose to go outside and draw with crayons!");
+                    }
+                } 
+                else if (input8 == "b")
+                {
+                    if(input1 == "b" && input5 == "c" && input8 == "b")
+                    {
+                        System.out.println("You chose to go outside and draw with colored pencils!");
+                    }
+                }
+                else 
+                {
+                    System.out.println("Do you want to draw with crayons (a) or colored pencils (b)?");
+                    input8 = input.nextLine();
+                    main(new String[0]); //goes back to the beginning of sequence
+                }
             }
         }
  
